acd83be359a66d21552bca9b17a338599ee260ed
Jack Clarke <jackclarke@192.168.1.167>
2025-06-04 15:52:31 +0100

    strip some stuff from makefiles that isnt needed

--- common.config.mk
+++ common.config.mk
@@ -1,46 +1,11 @@
-# Copyright © 2021 Luther Systems, Ltd. All right reserved.
+# General project config for dockerized buf generation
 
-# config.mk
-#
-# General project configuration that configures make targets and tracks
-# dependency versions.
-
-# PROJECT and VERSION are attached to docker images and phylum deployment
-# artifacts created during the build process.
-PROJECT=sandbox
+PROJECT=proto-experiment
 VERSION=0.1.0-SNAPSHOT
-SERVICE_DIR=portal
 
-# The makefiles use docker images to build artifacts in this project.  These
-# variables configure the images used for builds.
+# Docker image version (using Luther BuildEnv)
 BUILDENV_TAG=v0.0.92
 
-# These variables control the version numbers for parts of the Luther platform
-# and should be kept up-to-date to leverage the latest platform features.
-# See release notes: https://docs.luthersystems.com/luther/platform/release-notes
-SUBSTRATE_VERSION=v2.205.0
-SHIROCLIENT_VERSION=${SUBSTRATE_VERSION}
-SHIROTESTER_VERSION=${SUBSTRATE_VERSION}
-NETWORK_BUILDER_VERSION=v0.0.2
-MARTIN_VERSION=v0.1.0
-
-# A golang module proxy server can greatly help speed up docker builds but the
-# official proxy at https://proxy.golang.org only works for public modules.
-# When your application needs private go module dependencies consider running a
-# local athens-proxy server with an ssh/http configuration which can access
-# private source repositories, otherwise set GOPRIVATE (or GONOPROXY and
-# GONOSUMDB) if private modules are needed.  Though be aware that GOPRIVATE
-# requires credentials (e.g. for github ssh) be available during builds which
-# complicates things considerably.
-# 		https://docs.gomods.io/
-# 		https://golang.org/ref/mod#private-modules
-GOPROXY ?= https://proxy.golang.org
-GOPRIVATE ?=
-GONOPROXY ?= ${GOPRIVATE}
-GONOSUMDB ?= ${GOPRIVATE}
-
-# These variables configure the Hyperledger Fabric image versions for running
-# the full test network.
-FABRIC_IMAGE_TAG=2.5.9
-FABRIC_CA_IMAGE_TAG=1.5.12
-BASE_IMAGE_TAG=0.4.22
+# Build image full name (assuming BUILD_IMAGE_API is still configured somewhere centrally)
+BUILD_IMAGE_API=luthersystems/buildenv-go-proto
+BUILD_IMAGE=${BUILD_IMAGE_API}:${BUILDENV_TAG}
